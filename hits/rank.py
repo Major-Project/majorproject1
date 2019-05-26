dcacba146173d69ee37ef6dcf59e492a3bff7ca3
Himanshu Singh <himanshusingh.singh06@gmail.com>
2019-05-26 11:54:53 +0530

    Update rank.py

--- hits/rank.py
+++ hits/rank.py
@@ -5,10 +5,6 @@ from sys import stdout
 from math import sqrt
 from math import pow
 
-#Document represents a single document
-#a document must be created with a url
-#Document stores a list of outbound and inbound links of other Documents
-#it also stores a hub and authority score
 class Document:
     """Represents an HTML document"""
     def __init__(self, url):
@@ -18,8 +14,6 @@ class Document:
         self.hub = 1
         self.auth = 1
 
-#accepts a hashmap of url to Document objects
-#writes the Document objects to stdout in a reasonable fashion
 def dumpDocuments(documents):
     for doc in documents:
         print documents[doc].url
@@ -32,9 +26,6 @@ def dumpDocuments(documents):
         print
     print "number of documents = " + str(len(documents))
 
-#accepts a hashmap of url to Document objects and an inputFileName
-#reads the input file, creating Document objects and storing
-#them in documents
 def processDocuments(documents, inputFileName):
     print("reading from input file...")
     inputFile = open(inputFileName, "r")
@@ -50,8 +41,6 @@ def processDocuments(documents, inputFileName):
             current.outbound.append(documents[line[3:-1]])
             documents[line[3:-1]].inbound.append(documents[current.url])
 
-#accepts a hashmap of url to Document objects and an int iterations
-#runs the HITS algorithm on the Document objects for iterations number of times
 def rankByHITS(documents, iterations):
     for i in range(0, iterations):
         stdout.write(str(i + 1) + " out of " + str(iterations) + " iterations complete!\r")
@@ -76,8 +65,6 @@ def rankByHITS(documents, iterations):
             documents[doc].hub = documents[doc].hub / norm
     stdout.write("\n")
 
-#accepts a hashmap of url to Documents object, and an output file name
-#sorts and writes the Documents to the output file in a reasonable format
 def printRanks(documents, outputFileName):
     print("writing to output file...")
     newList = sorted(documents, key=lambda doc: documents[doc].auth + documents[doc].hub, reverse=True)
@@ -88,7 +75,6 @@ def printRanks(documents, outputFileName):
         rank = rank + 1
     outputFile.close()
 
-#prints usage information for rank to stdout
 def showUsage():
     print("rank usage:")
     print("   rank inputFile outputFile iterations")
@@ -96,7 +82,6 @@ def showUsage():
     print("   rank wikidump.txt out.txt 100")
     print("   rank wikidump4.txt out4.txt 15") 
 
-#yeah I'm used to java, learning python is hard
 def main():
     if (len(sys.argv) < 4):
         showUsage()
